719dc837f4e9e1d4455dc8ae5231eea507ae3184
burgessadrien <abb246@mun.ca>
2017-03-06 10:52:27 -0330

    finally got the list to work!!!!!  just making it clickable now

--- app/src/main/java/com/gradeslate/gradeslate/CoursesList.java
+++ app/src/main/java/com/gradeslate/gradeslate/CoursesList.java
@@ -2,15 +2,15 @@ package com.gradeslate.gradeslate;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ListViewCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -19,9 +19,15 @@ import com.gradeslate.gradeslate.backend.Courses;
 import java.util.ArrayList;
 
 public class CoursesList extends AppCompatActivity {
+
+    private FloatingActionButton fab;
+    private EditText mSemester;
     private ArrayList<Courses> semesters;
-    private String semester;
     private ArrayList<String> semesterNames;
+    private String semester;
+    private ArrayAdapter<String> adapter;
+    private EditText editText;
+    private String empty = "please add a semester";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,26 +36,49 @@ public class CoursesList extends AppCompatActivity {
         setSupportActionBar(toolbar);
         this.semesters = new ArrayList();
         this.semesterNames = new ArrayList();
+        semesterNames.add(empty);
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        ListView listview = (ListView) findViewById(R.id.semesterNames);
+        adapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.text_item, semesterNames);
+        listview.setAdapter(adapter);
+
+
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        onFabClick();
+    }
+
+    public void onFabClick(){
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 AlertDialog.Builder mBuilder = new AlertDialog.Builder(CoursesList.this);
                 View mView = getLayoutInflater().inflate(R.layout.dialog_semester, null);
-                final EditText mSemester = (EditText) mView.findViewById(R.id.editSemester);
+                mSemester = (EditText) mView.findViewById(R.id.editSemester);
                 Button mAdd = (Button) mView.findViewById(R.id.add);
                 mAdd.setOnClickListener(new View.OnClickListener()  {
                     @Override
                     public void onClick(View view) {
                         if(!mSemester.getText().toString().isEmpty()){
-                            Toast.makeText(CoursesList.this,
-                                    R.string.semesterAddedMsg, Toast.LENGTH_SHORT).show();
+                            // Toast.makeText(CoursesList.this,
+                            //       R.string.semesterAddedMsg, Toast.LENGTH_SHORT).show();
                             semester = mSemester.getText().toString();
-                            semesterNames.add(semester);
+                            //semesterNames.add(semester);
                             Courses newSemester = new Courses(semester);
                             semesters.add(newSemester);
+                            if(semesterNames.get(0)==empty) {
+                                semesterNames.clear();
+                                semesterNames.add(semester);
+                            }
+                            else{
+                                semesterNames.add(semester);
+                            }
+                            adapter.notifyDataSetChanged();
+                            String test = semesters.get(semesters.size()-1).getName();
+                            Toast.makeText(CoursesList.this,
+                                    test, Toast.LENGTH_SHORT).show();
+
                         }
                         else{
                             Toast.makeText(CoursesList.this,
@@ -60,30 +89,9 @@ public class CoursesList extends AppCompatActivity {
                 mBuilder.setView(mView);
                 AlertDialog dialog = mBuilder.create();
                 dialog.show();
+
             }
-
-
-
         });
-        ListAdapter semestersAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, semesterNames);
-        ListView semestersView = (ListView) findViewById(R.id.semestersView);
-        semestersView.setAdapter(semestersAdapter);
-
-        semestersView.setOnItemClickListener(
-                new AdapterView.OnItemClickListener(){
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id){
-                        String semester = semesterNames.get(position);
-                        //enter course page here
-                        //code to be added for implementation
-                    }
-                }
-        );
-
-    }//end of onCreate()
-
-
-
+    }
 
 }//end of class
-
